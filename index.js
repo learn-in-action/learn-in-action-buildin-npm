6e102bb6846172f3eb441ff48ff04adc67c4dbfd
postor <postor@gmail.com>
2017-04-30 12:05:01 +0800

    调整包结构

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
 const path = require('path')
 
-module.exports.dependences = []
-module.exports.chapters = [
-  {
-    title: '安装NPM(NodeJS)',
-    steps: [
-      {
-        title:'打开NodeJS官网',
-        desc: '<a href="https://nodejs.org/en/">NodeJS官网</a>',
-        page: path.join(__dirname,'resources','openurl.html'), 
-        validate: false,
-      },
-      {
-        title:'下载NodeJS',
-        desc: '点击下载并等待下载结束',
-        page: path.join(__dirname,'resources','download.html'), 
-        validate: false,
-      },
-      {
-        title:'安装NodeJS',
-        desc: '<a href="https://nodejs.org/en/">NodeJS官网</a>',
-        page: path.join(__dirname,'resources','install.html'), 
-        validate: 'npm -v',
-      },
-    ],
-  },
-]
+module.exports = {
+  title: '安装NPM(NodeJS)',
+  dependences: [],
+  chapters: [
+    {
+      title: '安装NPM(NodeJS)',
+      steps: [
+        {
+          title:'打开NodeJS官网',
+          desc: '<a href="https://nodejs.org/en/">NodeJS官网</a>',
+          page: path.join(__dirname,'resources','openurl.html'), 
+          validate: false,
+        },
+        {
+          title:'下载NodeJS',
+          desc: '点击下载并等待下载结束',
+          page: path.join(__dirname,'resources','download.html'), 
+          validate: false,
+        },
+        {
+          title:'安装NodeJS',
+          desc: '<a href="https://nodejs.org/en/">NodeJS官网</a>',
+          page: path.join(__dirname,'resources','install.html'), 
+          validate: 'npm -v',
+        },
+      ],
+    },
+  ]
+}
